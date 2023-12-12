43e614cf7f8eff2bc6593b76374d3c9fa7b9a89a
deva sahithiyan <devasahithiyan@gmail.com>
2023-12-12 09:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //document.querySelector("h2").innerHTML = "";
 
-var w = new Audio("sounds/crash.mp3");
-var a = new Audio("sounds/kick-bass.mp3");
-var s = new Audio("sounds/snare.mp3");
-var d = new Audio("sounds/tom-1.mp3");
-var j = new Audio("sounds/tom-2.mp3");
-var k = new Audio("sounds/tom-3.mp3");
-var l = new Audio("sounds/tom-4.mp3");
+var w = new Audio("crash.mp3");
+var a = new Audio("kick-bass.mp3");
+var s = new Audio("snare.mp3");
+var d = new Audio("tom-1.mp3");
+var j = new Audio("tom-2.mp3");
+var k = new Audio("tom-3.mp3");
+var l = new Audio("tom-4.mp3");
 for(let i = 0 ; i < document.querySelectorAll(".drum").length ; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function(){
        
